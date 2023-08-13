b63a1e4e02005080e6cd1672e4c8c3f824d52d4b
Maznitsina <maznicinaanna@gmail.com>
2023-08-13 18:50:18 +0300

    CommentController added

--- checking-sleep/src/main/java/ru/dream/checkingsleep/controller/CommentController.java
+++ checking-sleep/src/main/java/ru/dream/checkingsleep/controller/CommentController.java
@@ -1,4 +1,23 @@
 package ru.dream.checkingsleep.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import ru.dream.checkingsleep.dto.CommentDto;
+import ru.dream.checkingsleep.model.Dream;
+import ru.dream.checkingsleep.service.CommentService;
+@RestController
+@RequiredArgsConstructor
 public class CommentController {
+
+    private final CommentService commentService;
+
+    @GetMapping("/comment/{id}")
+    CommentDto getCommentByDream(@PathVariable("dream") Dream dream) {
+
+        return commentService.getCommentByDream(dream);
+    }
+
+
 }
